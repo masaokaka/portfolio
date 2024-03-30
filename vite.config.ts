4c9c211c2922b1ad0f3ca89070042116583786c5
masaokaka <osushiiisensu@gmail.com>
2024-03-30 18:27:31 +0900

    update: dev環境でtestとcssを無視するよう設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import { vitePlugin as remixVitePlugin } from '@remix-run/dev';
 import react from '@vitejs/plugin-react';
-import { installGlobals } from '@remix-run/node';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
-installGlobals();
 
 const isStorybook = process.argv[1]?.includes('storybook');
 
 export default defineConfig({
   plugins: [
-    !process.env.VITEST && !isStorybook ? remixVitePlugin() : react(),
+    !process.env.VITEST && !isStorybook
+      ? remixVitePlugin({
+          ignoredRouteFiles: [
+            '**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
+            '**/*.css',
+          ],
+        })
+      : react(),
     tsconfigPaths(),
   ],
   test: {
@@ -19,5 +23,6 @@ export default defineConfig({
     setupFiles: ['./test/setup-test-env.ts'],
     include: ['./app/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     watchExclude: ['.*\\/node_modules\\/.*', '.*\\/build\\/.*'],
+    env: loadEnv('test', process.cwd(), ''),
   },
 });
