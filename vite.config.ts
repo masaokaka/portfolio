f21ba5af29d78446736deddfa10e93fe8bf6c39f
masaokaka <osushiiisensu@gmail.com>
2024-03-30 22:09:54 +0900

    add: remix-flat-routesを追加し、configに設定を記載

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,31 @@ import { vitePlugin as remixVitePlugin } from '@remix-run/dev';
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { flatRoutes } from 'remix-flat-routes';
 
 const isStorybook = process.argv[1]?.includes('storybook');
 
+/**
+ * @type {import("@remix-run/dev").AppConfig}
+ */
 export default defineConfig({
   plugins: [
     !process.env.VITEST && !isStorybook
       ? remixVitePlugin({
-          ignoredRouteFiles: [
-            '**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
-            '**/*.css',
-          ],
+          ignoredRouteFiles: ['**/*'],
+          serverModuleFormat: 'esm',
+          routes: async (defineRoutes) => {
+            return flatRoutes('routes', defineRoutes, {
+              ignoredRouteFiles: [
+                '.*',
+                '**/*.css',
+                '**/*.test.{js,jsx,ts,tsx}',
+                '**/__*.*',
+                '**/*.server.*',
+                '**/*.client.*',
+              ],
+            });
+          },
         })
       : react(),
     tsconfigPaths(),
