e072ef694a7f7cce0365e24e14ec9bf47c9575d1
masaokaka <osushiiisensu@gmail.com>
2024-04-03 13:30:20 +0900

    update: actionsにテスト実行の処理を追記

--- app/routes/_index.test.tsx
+++ app/routes/_index.test.tsx
@@ -2,15 +2,22 @@ import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Index from '~/routes/_index';
 import { createRemixStub } from '@remix-run/testing';
+import { GeneralErrorBoundary } from '~/components/error-boundary';
+import { ThemeProvider } from '~/provider/theme';
 
 const RemixStub = createRemixStub([
   {
     path: '/',
-    Component: Index,
+    Component: () => (
+      <ThemeProvider>
+        <Index />
+      </ThemeProvider>
+    ),
+    ErrorBoundary: GeneralErrorBoundary,
   },
 ]);
 
-test('test1', () => {
+test('test if the top page renders properly.', () => {
   render(<RemixStub />);
-  expect(screen.getByText('Welcome to Remix')).toBeInTheDocument();
+  expect(screen.getByText('Name')).toBeInTheDocument();
 });
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,11 @@
 import type { MetaFunction } from '@remix-run/node';
-import { Loading } from '~/components/loading';
+// import { Loading } from '~/components/loading';
 import { ToggleButton } from '~/components/toggle-button';
-import { Theme, useTheme } from '~/utils/theme-provider';
+import { Theme, useTheme } from '~/provider/theme';
+import { Card } from '~/components/card';
+import { Text } from '~/components/text';
+import { Button } from '~/components/button';
+import { Title } from '~/components/title';
 
 export const meta: MetaFunction = () => {
   return [
@@ -17,14 +21,51 @@ export default function Index() {
     setTheme((prev) => (prev === Theme.DARK ? Theme.LIGHT : Theme.DARK));
   };
   return (
-    <div
-      style={{
-        fontFamily: 'system-ui, sans-serif',
-        lineHeight: '1.8',
-      }}
-    >
-      <Loading />
-      <ToggleButton enabled={isDarkMode} handleToggle={toggleTheme} />
-    </div>
+    <main>
+      <div className="grid grid-cols-3 grid-rows-4 gap-4">
+        <div className="col-span-2">
+          <Card>
+            <ToggleButton enabled={isDarkMode} handleToggle={toggleTheme} />
+            <Title>Name</Title>
+            <Text>こんにちは</Text>
+            <Button handleClick={() => console.log('click')}>Button</Button>
+          </Card>
+        </div>
+        <div className="col-span-1">
+          <Card>BBBBB</Card>
+        </div>
+        <div>
+          <Card>BBBBB</Card>
+        </div>
+        <div>
+          <Card>BBBBB</Card>
+        </div>
+        <div>
+          <Card>BBBBB</Card>
+        </div>
+        <div>
+          <Card>BBBBB</Card>
+        </div>
+        <div>
+          <Card>BBBBB</Card>
+        </div>
+        <div>
+          <Card>BBBBB</Card>
+        </div>
+        <div>
+          <Card>BBBBB</Card>
+        </div>
+        <div>
+          <Card>BBBBB</Card>
+        </div>
+        <div>
+          <Card>BBBBB</Card>
+        </div>
+        <div>
+          <Card>BBBBB</Card>
+        </div>
+      </div>
+      {/* <Loading /> */}
+    </main>
   );
 }
