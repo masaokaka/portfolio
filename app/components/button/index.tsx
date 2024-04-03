a42c2975c16c7bca6266466a4ffc9b19c77250ba
masaokaka <osushiiisensu@gmail.com>
2024-04-03 11:45:49 +0900

    update: buttonコンポーネント修正

---
+++ app/components/button/index.tsx
@@ -0,0 +1,20 @@
+import { PropsWithChildren } from 'react';
+
+type PropsButton = {
+  handleClick: () => void;
+};
+
+export const Button = ({
+  children,
+  handleClick,
+}: PropsWithChildren<PropsButton>) => {
+  return (
+    <button
+      type="button"
+      className="inline-flex items-center rounded-lg bg-primary px-4 py-2 text-center font-bold text-white"
+      onClick={handleClick}
+    >
+      {children}
+    </button>
+  );
+};
